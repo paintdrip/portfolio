ba5a53d51e46a256174f0c4b7f93f1e05bc740c3
Антон Кульбицкий <akulbitskiy@yandex.ru>
2025-02-05 11:35:34 +0300

    base header and pages

---
+++ components/Nav.jsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const links = [
+	{
+		name: 'главная',
+		path: '/',
+	},
+	{
+		name: 'проекты',
+		path: '/services',
+	},
+	{
+		name: 'резюме',
+		path: '/resume',
+	},
+	{
+		name: 'работа',
+		path: '/work',
+	},
+	{
+		name: 'инфо',
+		path: '/contact',
+	},
+]
+
+const Nav = () => {
+	const pathname = usePathname()
+
+	return (
+		<nav className='flex gap-8'>
+			{links.map((link, index) => {
+				return (
+					<Link key={index} href={link.path} className={`${link.path === pathname && 'text-accent border-b-2 border-accent'} capitalize font-medium hover:text-accent transition-all select-none`}>
+						{link.name}
+					</Link>
+				)
+			})}
+		</nav>
+	)
+}
+
+export default Nav

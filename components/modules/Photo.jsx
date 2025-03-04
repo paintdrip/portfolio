30dc0c0226d8077263c78fbd1c9721652f727d58
Anton <pa1nkller8879@gmail.com>
2025-03-04 15:58:20 +0300

    fix photo sizes

--- components/modules/Photo.jsx
+++ components/modules/Photo.jsx
@@ -9,7 +9,7 @@ export const Photo = () => {
 			<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2, duration: 0.4, ease: 'easeIn' } }}>
 				{/* image */}
 				<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' } }} className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute'>
-					<Image src='/assets/photo.png' priority quality={100} fill alt='' className='object-contain rounded-[50%]' />
+					<Image src='/assets/photo.png' priority quality={100} fill alt='' className='object-contain rounded-[50%] p-[7px] xl:p-[22px]' />
 				</motion.div>
 				{/* circle */}
 				<motion.svg className='w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]' fill='transparent' viewBox='0 0 506 506' xmlns='http://www.w3.org/2000/svg'>
